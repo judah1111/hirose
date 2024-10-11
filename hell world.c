b556d7bb6476b65ad9304c626825ca0ec75a3300
judah1111 <t236030@st.yamagata-u.ac.jp>
2024-10-11 19:10:54 +0900

    Add files via upload

---
+++ hell world.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int devil, number, devilnumber;
+    printf("Hell World\n");
+    devil = 333;
+    number = 333;
+    devilnumber = devil + number;
+    printf("The devil's number is %d\n", devilnumber);
+
+    return 0;
+}
